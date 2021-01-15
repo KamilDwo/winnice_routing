4e878dbf85306a78482eec5d2a5f426007baac8c
AILLERON\kadw <kadw@ailleron.com>
2021-01-15 09:39:07 +0100

    no message

--- app.js
+++
@@ -1,1 +0,0 @@
-The req (request) and res (response) are the exact same objects that Node provides, so you can invoke req.pipe(), req.on('data', callback), and anything else you would do without Express involved.
