c4fec17b0c84bd0c4298be81432a8e02d0966691
AILLERON\kadw <kadw@ailleron.com>
2021-02-08 12:33:42 +0100

    dodanie listy szlakow winnych per winnica

---
+++ helpers/listToArray.helper.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const listToArray = (fullString, separator) => {
+    let fullArray = [];
+
+    if (fullString !== undefined && fullString !== '' && fullString !==  null) {
+        if (fullString.indexOf(separator) === -1) {
+            fullArray.push(parseFloat(fullString));
+        } else {
+            fullArray = fullString.split(separator);
+            fullArray = fullArray.map(arrayItem => parseInt(arrayItem));
+        }
+    }
+    return fullArray;
+}
+
+module.exports = listToArray;
--- models/vineyard.models.js
+++ models/vineyard.models.js
@@ -2,40 +2,47 @@
 
 const connection = require('../config/db.config');
 const speakingurl = require('speakingurl');
+const listToArray = require('../helpers/listToArray.helper');
+
 
 const Vineyard = vineyard => {
     this.id = vineyard.id;
     this.name = vineyard.name;
 };
 
-const defaultFields = 'id,' +
-    ' name,' +
-    ' date_add as \'dateAdd\',' +
-    ' location,' +
-    ' province_id as \'provinceId\',' +
-    ' image_1 as \'image1\',' +
-    ' tastings, ' +
-    ' sightseeing, ' +
-    ' meals, ' +
-    ' events, ' +
-    ' additional, ' +
-    ' accommodation, ' +
-    ' sale, ' +
-    ' marker_id as \'markerId\'';
+const defaultFields = 'pw_vineyard.id,' +
+    ' pw_vineyard.name,' +
+    ' pw_vineyard.date_add as \'dateAdd\',' +
+    ' pw_vineyard.location,' +
+    ' pw_vineyard.province_id as \'provinceId\',' +
+    ' pw_vineyard.image_1 as \'image1\',' +
+    ' pw_vineyard.tastings, ' +
+    ' pw_vineyard.sightseeing, ' +
+    ' pw_vineyard.meals, ' +
+    ' pw_vineyard.events, ' +
+    ' pw_vineyard.additional, ' +
+    ' pw_vineyard.accommodation, ' +
+    ' pw_vineyard.sale, ' +
+    ' GROUP_CONCAT(pw_vineyard_paths.path_id) AS paths, ' +
+    ' pw_vineyard.marker_id as \'markerId\'';
+
+
 
 Vineyard.findAll = result => {
-    connection.query(`SELECT ${defaultFields} FROM pw_vineyard`, function (err, res) {
+    connection.query(`SELECT ${defaultFields} FROM pw_vineyard LEFT JOIN pw_vineyard_paths ON pw_vineyard.id=pw_vineyard_paths.vineyard_id GROUP BY pw_vineyard.id`, function (err, res) {
         if (err) {
             console.log('error: ', err);
             result(err);
         } else {
             const parseItems = res.map(item => {
                 item.features = [];
-                const location = item['location'].split(',');
+                const location = item.location.split(',');
+                const paths = listToArray(item.paths, ',');
                 item.isActive = item.isActive === 2;
-                const objUrl = `${item['id']}-${speakingurl(item['name'], [])}`;
+                const objUrl = `${item.id}-${speakingurl(item.name, [])}`;
                 item.location = [parseFloat(location[0]), parseFloat(location[1].replace(/\s+/g, ''))];
                 item.url = objUrl;
+                item.paths = paths;
                 return item;
             });
             result(parseItems);
