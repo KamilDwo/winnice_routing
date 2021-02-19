68f06c3ee85ec5f318817c39b7a1f32d6d488417
AILLERON\kadw <kadw@ailleron.com>
2021-02-19 09:08:23 +0100

    no message

--- models/instagram.models.js
+++ models/instagram.models.js
@@ -7,6 +7,7 @@ const Instagram = () => {}
 Instagram.getPhotosByUsername = (result, body) => {
     ;(async () => {
         await client.login()
+        console.log('klient: ', client);
         const profile = await client.getProfile()
         console.log(profile);
         result(profile)
