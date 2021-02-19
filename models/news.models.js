6cb103239e78828f88db963b20ecaa728f326014
AILLERON\kadw <kadw@ailleron.com>
2021-02-19 09:55:19 +0100

    no message

--- models/news.models.js
+++ models/news.models.js
@@ -41,8 +41,7 @@ News.getInstagramPhotos = (result, body) => {
     ;(async () => {
         await instagramClient.login()
         const profile = await instagramClient.getPhotosByUsername({ username: 'kamil.dwo' })
-        if (profile)
-            result(profile)
+        result(profile)
     })()
 };
 
