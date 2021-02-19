24195847cf121227505637599af0f3d507cc46bc
AILLERON\kadw <kadw@ailleron.com>
2021-02-19 09:06:33 +0100

    no message

--- config/instagram.config.js
+++ config/instagram.config.js
@@ -3,6 +3,7 @@
 require('dotenv').config()
 const Instagram = require('instagram-web-api')
 const { INSTAGRAM_USERNAME, INSTAGRAM_PASSWORD } = process.env
-
+console.log(INSTAGRAM_USERNAME, INSTAGRAM_PASSWORD);
 const instagramClient = new Instagram({ INSTAGRAM_USERNAME, INSTAGRAM_PASSWORD })
+console.log(instagramClient);
 module.exports = instagramClient
