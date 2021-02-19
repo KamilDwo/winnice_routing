4f66bd365241bddb0b9847656446a563acace3d9
AILLERON\kadw <kadw@ailleron.com>
2021-02-19 09:12:24 +0100

    no message

--- models/instagram.models.js
+++ models/instagram.models.js
@@ -3,7 +3,7 @@
 require('dotenv').config()
 const InstagramApi = require('instagram-web-api')
 const { INSTAGRAM_USERNAME, INSTAGRAM_PASSWORD } = process.env
-const instagramClient = new InstagramApi({ INSTAGRAM_USERNAME, INSTAGRAM_PASSWORD })
+const instagramClient = new InstagramApi({ username: INSTAGRAM_USERNAME, password: INSTAGRAM_PASSWORD })
 
 
 
