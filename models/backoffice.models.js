31f3accbd6ce7a7b64179bffccbd22914262adeb
AILLERON\kadw <kadw@ailleron.com>
2021-03-09 12:53:51 +0100

    no message

--- models/backoffice.models.js
+++ models/backoffice.models.js
@@ -2,7 +2,6 @@
 
 require('dotenv').config()
 const connection = require('../config/db.config')
-const listToArray = require('../helpers/listToArray.helper')
 
 const BackOffice = news => {
     this.id = news.id
@@ -22,20 +21,18 @@ BackOffice.getAllVineyards = (result, body) => {
         ' pw_vineyard.date_add as \'dateAdd\',' +
         ' pw_vineyard.province_id as \'provinceId\',' +
         ' pw_vineyard.is_active as \'isActive\', ' +
-        ' GROUP_CONCAT(pw_vineyard_paths.path_id) AS paths,' +
-        ' GROUP_CONCAT(pw_vineyard_organizations.organization_id) AS organizations' +
+        ' COUNT(pw_vineyard_organizations.id) as \'organisations\', ' +
+        ' COUNT(pw_vineyard_paths.id) as \'paths\', ' +
         '';
 
     connection.query(`SELECT ${defaultFields} FROM pw_vineyard
      LEFT JOIN pw_vineyard_paths ON pw_vineyard.id=pw_vineyard_paths.vineyard_id
-     LEFT JOIN pw_vineyard_organizations ON pw_vineyard.id=pw_vineyard_organizations.vineyard_id      
+      LEFT JOIN pw_vineyard_organizations ON pw_vineyard.id=pw_vineyard_organizations.vineyard_id
       GROUP BY pw_vineyard.id`, function (error, results) {
         if (error) {
             result(error, null)
         } else {
             const parseItems = results.map(item => {
-                item.paths = listToArray(item.paths, ',').length
-                item.organizations = listToArray(item.organizations, ',').length
                 item.isActive = item.isActive === 2
                 return item
             });
