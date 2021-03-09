1fb7834685fb4e9d4aa8d1d23acff7634cabd344
AILLERON\kadw <kadw@ailleron.com>
2021-03-09 13:06:11 +0100

    no message

--- models/backoffice.models.js
+++ models/backoffice.models.js
@@ -22,17 +22,18 @@ BackOffice.getAllVineyards = (result, body) => {
         ' pw_vineyard.province_id as \'provinceId\',' +
         ' pw_vineyard.is_active as \'isActive\', ' +
         ' COUNT(pw_vineyard_paths.id) as \'paths\' ' +
+        ' COUNT(pw_vineyard_organizations.id) as \'organizations\' ' +
         '';
 
     connection.query(`SELECT ${defaultFields} FROM pw_vineyard
      LEFT JOIN pw_vineyard_paths ON pw_vineyard_paths.vineyard_id=pw_vineyard.id
+     LEFT JOIN pw_vineyard_organizations ON pw_vineyard_organizations.vineyard_id=pw_vineyard.id
       GROUP BY pw_vineyard.id`, function (error, results) {
         if (error) {
             result(error, null)
         } else {
             const parseItems = results.map(item => {
                 item.isActive = item.isActive === 2
-                item.organizations = 0;
                 return item
             });
             result(parseItems, null)
