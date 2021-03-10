77763baae1c893af80a9429660636fea134e90f8
AILLERON\kadw <kadw@ailleron.com>
2021-03-10 23:02:47 +0100

    no message

--- routes/backOffice.routes.js
+++ routes/backOffice.routes.js
@@ -9,6 +9,20 @@ const corsOptionsDelegate = require('../config/cors.config');
 router.get('/vineyards', cors(corsOptionsDelegate), backOfficeController.getAllVineyards);
 router.post('/login', cors(corsOptionsDelegate), backOfficeController.loginAdmin);
 router.get('/vineyards/:id', cors(corsOptionsDelegate), backOfficeController.getVineyardById);
-router.post('/upload_photo', cors(corsOptionsDelegate), backOfficeController.uploadPhoto);
+router.post('/upload_photo', cors(corsOptionsDelegate), (req, res, next) => {
+    let uploadFile = req.files.file
+    const fileName = req.files.file.name
+    uploadFile.mv(
+        `${__dirname}/public/files/${fileName}`,
+        function (err) {
+            if (err) {
+                return res.status(500).send(err)
+            }
+            res.json({
+                file: `public/${req.files.file.name}`,
+            })
+        },
+    )
+})
 
 module.exports = router;
