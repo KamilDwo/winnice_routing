ca99062e9d913c6753fdcd4046e707b665992692
Kamil Dworak <kamil.dworak@allegro.pl>
2021-09-07 16:46:10 +0200

    fix backoffice winetypes request field

--- models/backoffice.models.js
+++ models/backoffice.models.js
@@ -645,7 +645,7 @@ BackOffice.getWineById = (id, result) => {
             SELECT wines_meals.mealId, meals.name FROM wines_meals LEFT JOIN meals ON wines_meals.mealId = meals.id WHERE wines_meals.wineId = ? GROUP BY wines_meals.mealId;
             SELECT wines_provinces.provinceId, provinces.name FROM wines_provinces LEFT JOIN provinces ON provinces.id = wines_provinces.provinceId WHERE wines_provinces.wineId = ? GROUP BY wines_provinces.provinceId;
             SELECT wines_additional_features.featureId FROM wines_additional_features WHERE wines_additional_features.wineId = ?;
-            SELECT wines_strains.strainId, winetypes.title, winetypes.colour FROM wines_strains LEFT JOIN winetypes ON winetypes.id = wines_strains.strainId WHERE wines_strains.wineId = ? GROUP BY wines_strains.strainId;
+            SELECT wines_strains.strainId, winetypes.name, winetypes.colour FROM wines_strains LEFT JOIN winetypes ON winetypes.id = wines_strains.strainId WHERE wines_strains.wineId = ? GROUP BY wines_strains.strainId;
             SELECT vineyards_wines.vineyardId, vineyards.name FROM vineyards_wines LEFT JOIN vineyards ON vineyards_wines.vineyardId = vineyards.id WHERE vineyards_wines.wineId = ? GROUP BY vineyards_wines.vineyardId;
         `;
 
